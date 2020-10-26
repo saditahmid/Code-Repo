73093b4a927d65cebbbb6838138d081e79fc846d
Tahmid Hossain <1820228@iub.edu.bd>
2020-10-26 09:00:20 +0600

    Create LastDigit.cpp

---
+++ CSC101/Operations/LastDigit.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int x,y;
+    cout<< "enter any number=";
+    cin>>y;
+
+    x=y%10;
+    cout<< "the last digit of the number is=" <<x<<endl;
+
+    return 0;
+}
+
