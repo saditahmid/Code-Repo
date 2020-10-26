ca70e51e4867a3e570b83137910b6d6b4dcace7c
Tahmid Hossain <1820228@iub.edu.bd>
2020-10-26 09:00:53 +0600

    Create Int_Float.cpp

---
+++ CSC101/Operations/Int_Float.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int y;
+    float x,z;
+    cout<< "enter any integer=";
+    cin>>x;
+
+    y=x;
+    z=(x-y);
+    cout<< "Integer part=" <<y<< endl<<"decimal part=" <<z<< endl;
+
+    return 0;
+}
