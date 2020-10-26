c49006cf08571156dcceddbcfa6aa75fcd0d4ae7
Tahmid Hossain <1820228@iub.edu.bd>
2020-10-26 08:53:14 +0600

    Create Circle.cpp
    A C++ program that finds the Area and Circumference of a Circle

---
+++ CSC101/Operations/Circle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+      float r,area,circum;
+      cin>>r;
+
+      area=r*r*3.142;
+      cout<<"Area of the circle is="<<area<<endl;
+
+    circum=2*3.142*r;
+    cout<<"Circumference of the circle is="<<circum<<endl;
+    return 0;
+}
